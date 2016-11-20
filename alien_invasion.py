af52558f0576fa7a587d35f24b402c8bd0d362f0
rayallen20 <452857163@qq.com>
2016-11-21 01:08:55 +0800

    在屏幕背景上添加了飞船
    ship.py:飞船类 代码下有注释 说明如何绘制飞船 以及如何指定飞船的位置
    ship.bmp: 飞船的位图 pygame貌似对其他图片支持不是很好 尽量使用位图
    alien_invasion.py: 在screen上绘制了一个飞船
    并解释了飞船的surface对象和screen的surface对象的互相遮挡关系

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -13,6 +14,9 @@ def run_game():
     )
     pygame.display.set_caption('Alien Invasion')
 
+    # 创建一艘飞船 必须在主while循环前创建Ship实例 否则将每次循环时都创建一艘飞船
+    ship = Ship(screen)
+
     # 开始游戏主循环
     while True:
 
@@ -22,7 +26,10 @@ def run_game():
                 sys.exit()
 
         # 每次循环时都重绘屏幕
+        # ship.blitme() 一定要在主屏幕绘制后再绘制 否则表示飞船的surface对象 会被主屏幕的srcface对象遮挡 就看不到飞船了
+        # 屏幕的遮挡 类似于 美术的 互相遮挡概念 即最上层的surface对象 要最后被绘制 有点类似内存中堆栈的概念
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 使最后一次绘制的屏幕可见
         pygame.display.flip()
---
+++ ship.py
@@ -0,0 +1,41 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将每艘新飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+
+"""
+解释:
+__init__()中的screen: 即self,screen 指定了将飞船绘制到什么地方
+pygame.image.load() 返回一个表示图片的surface 将这个surface给了self.image
+get_rect() 获取该surface的属性rect (rect本身也是一个对象)
+image.get_rect() 获取图片的rect对象
+screen.get_rect() 获取屏幕的rect对象
+get_rect()概述:
+也就是说 在代码中处理的 并不是 "飞船" 这个游戏元素 而是 "飞船图片" 这个rect形状
+处理rect对象时 使用矩形四角和中心的x,y坐标 通过设置这些值 来指定矩形的位置
+位置属性概述:
+center centerx centery 设置rect对象居中常用
+top bottom left right 设置rect对象与屏幕边缘对齐常用
+x y 调整rect对象的水平或垂直位置
+x y 坐标系概述:
+pygame中 以左上角为原点 水平向右为x轴正方向 垂直向下为y轴正方向
+self.rect.centerx = self.screen_rect.centerx 将飞船的中心点的x坐标 设置为 屏幕中心点的x坐标
+self.rect.bottom = self.screen_rect.bottom 将飞船下边缘的y坐标 设置为 屏幕下边缘的y坐标
+blitme()方法中  screen.blit(要绘制的图像surface, 背景surface) 即根据__init__中的rect的位置 在screen上绘制图像
+"""
